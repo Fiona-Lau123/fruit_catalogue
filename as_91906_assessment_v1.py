75474267c98ac4d4a63c537eda51b84cae4333e3
Fiona-Lau123 <fi210151@students.aggs.school.nz>
2025-04-04 06:17:36 +1300

    fruit_catalogue_v5

--- as_91906_assessment_v1.py
+++ as_91906_assessment_v1.py
@@ -2,19 +2,18 @@ import easygui
 import number_range as c
 import csv
 
-
-f = open("fruits.txt", "r")
-
+f = open("fruit.txt", "r")
 
 reader = csv.reader(f)
 
-
 fruits = {}
-
 
 for row in reader:
     if len(row) == 5:
-        fruits[row[0]] = {"Potassium": row[1], "Phosphorus": row[2], "Sugar": row[3], "Calories": row[4]}
+        fruits[row[0]] = {"Potassium": row[1], 
+                          "Phosphorus": row[2], 
+                          "Sugar": row[3], 
+                          "Calories": row[4]}
 
 
 def options():
@@ -22,18 +21,15 @@ def options():
         "Add fruit": add_fruit,
         "Search": search,
         "Show fruits": display_fruits,
-        # "Delete": delete,
+        "Delete": delete,
         "Exit": exit_fully,
     }
 
-
     input = "pick"
-
 
     # Loop below continues until user selects "Exit" from options and
     # get_input is set as "N"
     while input != "start":
-
 
         msg = "Welcome to Fruit Mix\n What would it be today?"
         title =  "Fruit mix"
@@ -43,17 +39,20 @@ def options():
          
         selection = easygui.buttonbox(msg, title, choices)
 
-        input = options[selection]()
 
+        input = options[selection]()
 
 def add_fruit():
     title = "Adding fruit"
     msg = "What fruit do you want to add?"
     added_fruit = easygui.enterbox(msg, title).upper()
 
+    # if added_fruit == None:
+    #     added_fruit = cancel()
 
+    while added_fruit == None:
+        added_fruit = cancel()
     fruit_check = existence(added_fruit)
-
 
     if fruit_check in fruits.keys():
         msg = added_fruit + " ALREADY EXIST"
@@ -62,22 +61,19 @@ def add_fruit():
         return add_fruit()
        
 
-
     elif added_fruit.isalpha():
         potassium_value = potassium_value_check()
         phosphorus_value = phosphorus_value_check()
         sugar_value = sugar_value_check()
         calories_value = calories_value_check()
-        print(potassium_value)
+        print(added_fruit)
+        
 
-
+    # value = fruits[added_fruit][]
     else:
         verify(added_fruit)
 
-
     # added_fruit_item =  fruits[added_fruit].split(",")
-    # print(fruits[added_fruit])
-
 
     fruits[added_fruit] = {
         "Potassium": potassium_value,
@@ -85,31 +81,45 @@ def add_fruit():
         "Sugar": sugar_value,
         "Calories": calories_value
     }
+    print(fruits)
     print(fruits[added_fruit])
-   
+
+    # fruits[added_fruit][]
     # f = open("fruits.txt", "w")
     # f.write(added_fruit + ",")
     # f.close
-
-
     fruit_details(added_fruit)
+
+# def fruit_dictionary(added_fruit):
+    
 
 def potassium_value_check():
    
     msg = "Add a potassium(mg) value"
     title = "Add value"
     potassium_value = easygui.enterbox(msg, title)
+
+
+    if potassium_value == None:
+        added_fruit = cancel()
+        print(potassium_value)
+        print(added_fruit)
+
+
    
-    if potassium_value.isdigit() == True or potassium_value != "":
+    elif potassium_value.isdigit() == True or potassium_value != "":
         while potassium_value == "" or int(potassium_value) < c.MIN_POTASSIUM or int(potassium_value) > c.MAX_POTASSIUM:
             msg = "Value entered for potassium needs to be in between " + str(c.MIN_POTASSIUM) + " to " + str(c.MAX_POTASSIUM)
             title = "Value Error"
             new_value = easygui.enterbox(msg, title)
 
 
+
+
             if int(new_value) >= c.MIN_POTASSIUM and int(new_value) <= c.MAX_POTASSIUM:
                 print(new_value)
                 return(new_value)
+    print(potassium_value)
     return(potassium_value)
            
 def phosphorus_value_check():
@@ -117,13 +127,11 @@ def phosphorus_value_check():
     title = "Add value"
     phosphorus_value = easygui.enterbox(msg, title)
 
-
     if phosphorus_value.isdigit() == True or phosphorus_value != "":
         while phosphorus_value == "" or int(phosphorus_value) < c.MIN_PHOSPHORUS or int(phosphorus_value) > c.MAX_PHOSPHORUS:
             msg = "Value entered for phosphorus(mg) needs to be in between " + str(c.MIN_PHOSPHORUS) + " to " + str(c.MAX_PHOSPHORUS)
             title = "Value Error"
             new_value = easygui.enterbox(msg, title)
-
 
             if int(new_value) >= c.MIN_PHOSPHORUS and int(new_value) <= c.MAX_PHOSPHORUS:
                 print(new_value)
@@ -136,7 +144,6 @@ def sugar_value_check():
     title = "Add value"
     sugar_value = easygui.enterbox(msg, title)
 
-
     if sugar_value.isdigit() == True or sugar_value != "":
         while sugar_value == "" or int(sugar_value) < c.MIN_SUGAR or int(sugar_value) > c.MAX_SUGAR:
             msg = "Value entered for sugar(g) needs to be in between " + str(c.MIN_SUGAR) + " to " + str(c.MAX_SUGAR)
@@ -147,13 +154,13 @@ def sugar_value_check():
             if int(new_value) >= c.MIN_SUGAR and int(new_value) <= c.MAX_SUGAR:
                 print(new_value)
                 return(new_value)
+            
     return(sugar_value)
-           
+
 def calories_value_check():
-    msg = "Add a calorie(kcal) value"
+    msg = "Add a phosphorus(mg) value"
     title = "Add value"
     calories_value = easygui.enterbox(msg, title)
-
 
     if calories_value.isdigit() == True or calories_value != "":
         while calories_value == "" or int(calories_value) < c.MIN_CALORIES or int(calories_value) > c.MAX_CALORIES:
@@ -161,13 +168,11 @@ def calories_value_check():
             title = "Value Error"
             new_value = easygui.enterbox(msg, title)
 
-
             if int(new_value) >= c.MIN_CALORIES and int(new_value) <= c.MAX_CALORIES:
                 print(new_value)
                 return(new_value)
-    
+            
     return(calories_value)
-
 
 
 def search():
@@ -177,11 +182,10 @@ def search():
    
     # Goes through the existence function to check whether the fruit exist or not
     fruit_check = existence(fruit_searched)
-
+    print(fruit_check)
 
     if fruit_check:
         fruit_details(fruit_check)
-
 
     else:
         msg =  fruit_searched + " DOES NOT EXIST"
@@ -189,9 +193,7 @@ def search():
         easygui.msgbox(msg, title)
     verify(fruit_searched)
 
-
 def display_fruits():
-
 
     for fruit_id, fruit_value in fruits.items():
         print("\nFruit name: " + fruit_id)
@@ -199,9 +201,7 @@ def display_fruits():
         for fruit_key in fruit_value:
             print(fruit_key + ":", fruit_value[fruit_key])
 
-
 def verify(fruit_name):
-
 
     while fruit_name == "" or fruit_name.isalpha() == False:
         msg = "Please enter a fruit name"
@@ -211,8 +211,8 @@ def verify(fruit_name):
         if new_searched.isalpha():
             return new_searched
 
-        # if new_searched =!
 
+        # if new_searched =!
 
 def existence(fruit_check):
 
@@ -224,50 +224,92 @@ def existence(fruit_check):
 def fruit_details(fruit_name):
     details = [fruit_name + " DETAILS \n"]
 
-
     for value in fruits[fruit_name]:
         price = fruits[fruit_name][(value)]
-        all_details = value + " :" + price
-        details.append(all_details)
+        all_details = value + " : " + price
+        full_details = details.append(all_details)
+    print(details)
    
     msg = "Do you want to make any changes"
     title = "Fruit details"
     choices = ["No", "Change"]
     changes = easygui.buttonbox("\n".join(details) + "\n\n" + msg, title, choices)
 
-
     if changes == "Change":
-        make_changes(fruit_name)
+        make_changes(fruit_name, details)
+
+    else:
+        msg = "Do you want to add another fruit?"
+        title = "Adding another fruit"
+        another_fruit = easygui.ynbox(msg, title)
+
+        if another_fruit:
+            add_fruit()
 
 
-def make_changes(fruit_name):
+def make_changes(fruit_name, details):
     edit_boxes = {"Potassium(mg)": potassium_value_check,
                   "Phosphorus(mg)": phosphorus_value_check,
                   "Sugar(g)": sugar_value_check,
-                  "Calories(Kcal)": calories_value_check
+                  "Calories(kcal)": calories_value_check
                   }
     msg = "What value do you want to change?"
     title = "Changing value"
     boxes = []
     for edit_options in edit_boxes:
         boxes.append(edit_options)
-
+    print(edit_options)
 
     box_choosed = easygui.buttonbox(msg, title, boxes)
     print(box_choosed)
 
+# value = fruits[fruit_name][box_choosed]
+#     print(value)
+   
+    value = edit_boxes[box_choosed]()
+    print(edit_boxes)
+    print(value)
 
-    changed = edit_boxes[box_choosed]()
-    print(changed)
-    fruits[fruit_name][box_choosed][changed]
 
-    print(fruits[fruit_name][box_choosed][changed])
-    
+    fruits[fruit_name].update({box_choosed: value})
+    print(fruits)
 
+    fruit_details(fruit_name)
+
+def delete():
+    msg = "What fruit would you like to delete?"
+    title = "Delete fruit"
+    delete_fruit = easygui.enterbox(msg, title).upper()
+
+    check_delete = existence(delete_fruit)
+    print(check_delete)
+    print(fruits.keys())
+
+    if check_delete in fruits.keys():
+        msg = "Are you sure you want to delete " + check_delete
+        title = "confirm delete"
+        delete = easygui.ynbox(msg, title)
+
+        if delete:
+            del fruits[delete_fruit]
+            print(fruits)
+
+def cancel():
+    msg = "Do you want to leave?"
+    title = "Leave"
+    leave = easygui.ynbox(msg, title)
+
+    if leave:
+        return options()
+    else:
+        return 0
+
+   
 def exit_fully():
     msg = "Are you sure you want to exit?"
     title = "Exiting?"
     exit = easygui.ynbox(msg,title)
+
     if exit:
         return "start"
    
@@ -275,4 +317,3 @@ def exit_fully():
         return options
    
 options()
-
